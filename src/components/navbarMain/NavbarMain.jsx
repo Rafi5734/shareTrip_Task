a936b100921c46357136a7e51329dbe5460fff29
Rafi5734 <kamrulhasun5734@gmail.com>
2024-05-28 20:41:23 +0600

    navbar changed

--- src/components/navbarMain/NavbarMain.jsx
+++ src/components/navbarMain/NavbarMain.jsx
@@ -22,7 +22,7 @@ const NavbarMain = () => {
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 me-3"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded="true"
           >
             <span className="sr-only">Open main menu</span>
             <svg
